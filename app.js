65f63866a1484b4b94db30b58dc5948c12844fe8
CrocoDillon <dillon@crocodillon.com>
2014-07-07 16:36:13 -0400

    Complete styling and schedule, no more MongoDB

--- app.js
+++ app.js
@@ -3,9 +3,7 @@
  */
 
 var express      = require('express'),
-    mongoose     = require('mongoose'),
     path         = require('path'),
-    fs           = require('fs'),
     //favicon    = require('serve-favicon'),
     logger       = require('morgan'),
     bodyParser   = require('body-parser'),
@@ -17,28 +15,6 @@ var express      = require('express'),
 
 var app = express(),
     config = require('./config/config')[app.get('env')];
-
-// mongoose db connection
-function connect() {
-  mongoose.connect(config.db, {
-    server: { socketOptions: { keepAlive: 1 } }
-  });
-}
-connect();
-mongoose.connection.on('error', function(err) {
-  console.error('Mongoose error: ', err);
-});
-mongoose.connection.on('disconnected', function() {
-  console.log('Mongoose disconnected... reconnecting!');
-  connect();
-});
-
-// bootstrap models
-var models = path.join(__dirname, 'app', 'models');
-fs.readdirSync(models).forEach(function(file) {
-  if (~file.indexOf('.js'))
-    require(path.join(models, file));
-});
 
 // express setup
 app.set('port', config.port);
--- app/controllers/wedding.js
+++ app/controllers/wedding.js
@@ -2,49 +2,72 @@
  * Module dependencies.
  */
 
-var router   = require('express').Router(),
-    mongoose = require('mongoose');
-
-var Locations = mongoose.model('Location'),
-    Events    = mongoose.model('Event');
+var router   = require('express').Router();
 
 router.get('/', function(req, res) {
-  var guest, locations, events
-      callback = function() {
-        if (locations && events) {
-          res.render('wedding/index', {
-            guest: guest,
-            locations: locations,
-            events: events
-          });
-        }
-      };
+  var guest, events;
 
   switch (req.cookies['wedding.access']) {
     case 'party':
       guest = 'Party Guest';
+      events = [
+        {
+          time: '15:30',
+          name: 'Deuren Koninkrijkszaal open',
+          location: 'Koninkrijkszaal'
+        }, {
+          time: '16:00',
+          name: 'Huwelijkslezing'
+        }, {
+          time: '19:30',
+          name: 'Deuren Partycentrum open',
+          location: 'Charisma Partycentrum'
+        }, {
+          time: '20:00',
+          name: 'Eerste dans!'
+        }, {
+          time: '0:00',
+          name: 'Bedankt voor uw bezoek!'
+        }
+      ];
       break;
     case 'vip':
       guest = 'V.I.P Guest';
+      events = [
+        {
+          time: '13:00',
+          name: 'Deuren Partycentrum open',
+          location: 'Charisma Partycentrum'
+        }, {
+          time: '13:30',
+          name: 'Het Ja-Woord :)'
+        }, {
+          time: '15:30',
+          name: 'Deuren Koninkrijkszaal open',
+          location: 'Koninkrijkszaal'
+        }, {
+          time: '16:00',
+          name: 'Huwelijkslezing'
+        }, {
+          time: '17:30',
+          name: 'Diner en feestelijke afsluiting',
+          location: 'Charisma Partycentrum'
+        }, {
+          time: '20:00',
+          name: 'Eerste dans!'
+        }, {
+          time: '0:00',
+          name: 'Bedankt voor uw bezoek!'
+        }
+      ];
       break;
     default:
       return res.status(403).render('wedding/403');
   }
 
-  Locations.list({}, function(err, data) {
-    if (err)
-      return res.render('error/500');
-
-    locations = data;
-    callback();
-  });
-
-  Events.list({}, function(err, data) {
-    if (err)
-      return res.render('error/500');
-
-    events = data;
-    callback();
+  res.render('wedding/index', {
+    guest: guest,
+    events: events
   });
 });
 
